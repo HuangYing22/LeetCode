42a42c52e591220e823b9b9599387670b8325b07
HuangYing22 <huangying19880522@gmail.com>
2016-01-16 22:51:06 -0800

    Update LC009.cpp

--- LC009.cpp
+++ LC009.cpp
@@ -1,5 +1,13 @@
 // LC 9
 // Determine whether an integer is a palindrome. Do this without extra space.
+// Could negative integers be palindromes? (ie, -1)
+// If you are thinking of converting the integer to string, note the restriction of using extra space.
+// You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+// There is a more generic way of solving this problem.
+
+// INT_MAX = 2147483647
+// INT_MIN = -2147483648
+
 class Solution {
 public:
     bool isPalindrome(int x) {
@@ -8,7 +16,7 @@ public:
         
         int copy_x = x;
         int d;
-        int reverse_x = 0;
+        long long reverse_x = 0;
         
         while(x > 0) {
             d = x % 10;
@@ -16,6 +24,7 @@ public:
             x /= 10;
         }
         
+        if(reverse_x > INT_MAX) return false;
         return (copy_x == reverse_x);
     }
 };
