d545ef69a88c606b2ea2a31ad9e99f576e417b16
HuangYing22 <huangying19880522@gmail.com>
2017-02-22 17:06:53 -0800

    Create LC035.cpp

---
+++ LC035.cpp
@@ -0,0 +1,24 @@
+/* Given a sorted array and a target value, return the index if the target is found. 
+If not, return the index where it would be if it were inserted in order.
+
+You may assume no duplicates in the array.
+
+Here are few examples.
+[1,3,5,6], 5 → 2
+[1,3,5,6], 2 → 1
+[1,3,5,6], 7 → 4
+[1,3,5,6], 0 → 0 */
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int n = nums.size();
+        if((nums[0] > target) || (n == 0)) return 0;
+        
+        for(int i = 0; i < n; i ++) {
+            if (nums[i] >= target) return i;
+        }
+        
+        return n;
+    }
+};
