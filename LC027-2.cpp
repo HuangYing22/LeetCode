1f3fe3973b6cf705f0b02283abcc4c38f19a7703
HuangYing22 <huangying19880522@gmail.com>
2017-02-22 17:39:01 -0800

    Rename LC027.cpp to LC027-2.cpp

---
+++ LC027-2.cpp
@@ -0,0 +1,34 @@
+/* Given an array and a value, remove all instances of that value in place and return the new length.
+
+Do not allocate extra space for another array, you must do this in place with constant memory.
+
+The order of elements can be changed. It doesn't matter what you leave beyond the new length.
+
+Example:
+Given input array nums = [3,2,2,3], val = 3
+
+Your function should return length = 2, with the first two elements of nums being 2. */
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int n = nums.size();
+        
+        int l = 0;
+        int r = n - 1;
+        
+        while(l <= r) {
+            if(nums[r] == val) {
+                r --;
+            } else {
+                if(nums[l] == val) {
+                    nums[l] = nums[r];
+                    r --;
+                }
+                l ++;
+            }
+        }
+        
+        return r + 1;
+    }
+};
