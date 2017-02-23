d6123172d2fee8f80dd82b7946930a48dcac6b5d
HuangYing22 <huangying19880522@gmail.com>
2017-02-23 10:38:21 -0800

    Create LC268.cpp

---
+++ LC268.cpp
@@ -0,0 +1,21 @@
+/* Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, 
+find the one that is missing from the array.
+
+For example,
+Given nums = [0, 1, 3] return 2.
+
+Note:
+Your algorithm should run in linear runtime complexity. 
+Could you implement it using only constant extra space complexity? */
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        long long sum = (n * (n + 1))/2;
+        for(int i = 0; i < n; i ++)
+            sum -= nums[i];
+            
+        return (int)sum;
+    }
+};
